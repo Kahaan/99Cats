c753154684c45fa8c871b7eafda84ce96bb122f0
Kahaan <kahaan.r.patel@gmail.com>
2018-11-27 13:28:58 -0800

    adding controller#actions

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -5,6 +5,7 @@ class CatsController < ApplicationController
 
   def show
     @cat = Cat.find(params[:id])
+    render 'show'
   end
 
   def new
@@ -21,6 +22,16 @@ class CatsController < ApplicationController
   end
 
   def edit
+    @cat = Cat.find(params[:id])
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update(cat_params)
+      redirect_to @cat
+    else
+      render @cat.errors.full_messages
+    end
   end
 
 
